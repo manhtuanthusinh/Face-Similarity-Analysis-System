38744e00afbbb3635440e77807b3443f5b6b9667
Tuan Lai <tuanlaimanh20041@gmail.com>
2026-03-03 14:38:28 +0700

    feat: add example of a config.py file

---
+++ config_example.py
@@ -0,0 +1,11 @@
+# an example for config.py (you customize yourself)
+
+DATASET_PATH = "data/..."
+OUTPUT_CSV = "output/..."     # use for main.py
+MODEL_CKPT = "pretrained/..."
+DEVICE = "cpu"      # "cuda:0" if your computer has gpu
+MODEL_ARCHITECTURE = '...'
+OUTPUT_FEATURES_NPY = "output/features.npy"     # use for main1.py
+OUTPUT_LABELS_NPY = "output/labels.npy"         # use for main1.py
+
+OUTPUT_XLSX = "output/..."  # output excel (face_query_match.py)
