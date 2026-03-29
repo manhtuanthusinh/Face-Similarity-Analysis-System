1eafad82125f3f0f11c8dd59ecb1c07683479fd5
Tuan Lai <tuanlaimanh20041@gmail.com>
2026-03-29 18:10:44 +0700

    feat: add config file (example)

---
+++ config_example.py
@@ -0,0 +1,11 @@
+# config file
+
+DATASET_PATH = ""
+OUTPUT_CSV =  ""   
+MODEL_CKPT = "pretrained/adaface_ir18_webface4m.ckpt"
+DEVICE = "cpu"      # "cuda:0" if your computer has gpu
+MODEL_ARCHITECTURE = 'ir_18'
+OUTPUT_FEATURES_NPY = ""     
+OUTPUT_LABELS_NPY = ""         
+
+OUTPUT_XLSX = ""  # output excel
